60b24176f9ccd36b25a17a38bafc8a60e6f3dfaf
minrocha <min_rocha@hotmail.com>
2022-06-06 17:12:59 -0300

    exercicio modulos JS

---
+++ 00-praticas-com-javascript/15-dio-modulos-js/main.mjs
@@ -0,0 +1,5 @@
+import {mostrarCidade, mostrarIdade, mostrarHobby} from './funcoes.js';
+
+console.log(mostrarIdade('Maycon', 28));
+console.log(mostrarCidade('Maycon', 'Aracaju'));
+console.log(mostrarHobby('Maycon', 'Programar'));
