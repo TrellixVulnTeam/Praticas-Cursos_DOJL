bd6cb3027cd6f98a837bb8c87aaae349e4128424
minrocha <min_rocha@hotmail.com>
2022-07-14 17:14:40 -0300

    novoas atividades

---
+++ 00-praticas-com-javascript/16-date/script.js
@@ -0,0 +1,14 @@
+const d = new Date();
+
+console.log(d);
+console.log(d.toDateString());
+console.log(d.toUTCString());
+console.log(d.toString());
+
+const e = new Date(1994, 0, 7, 4, 27, 32);
+
+console.log(e);
+
+const f = new Date('2022-01-6 14:25:08');
+
+console.log(f);
